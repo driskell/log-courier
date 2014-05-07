5360a8563d3596dd500bb5922f37eb5d16d59814
Driskell <devel@jasonwoods.me.uk>
2014-05-07 22:30:18 +0100

    Add what/next option to multiline

--- codec_multiline.go
+++ codec_multiline.go
@@ -7,8 +7,12 @@ import (
   "strings"
 )
 
+const CODECMULTILINE_PREVIOUS = 0x00000001
+const CODECMULTILINE_NEXT = 0x00000002
+
 type CodecMultilineFactory struct {
   pattern string
+  what    uint32
   negate  bool
 }
 
@@ -32,12 +36,25 @@ func CreateCodecMultilineFactory(config map[string]interface{}) (*CodecMultiline
     } else if key == "pattern" {
       result.pattern, ok = value.(string)
       if !ok {
-        return nil, errors.New("Invalid value for 'pattern'.")
+        return nil, errors.New("Invalid value for 'pattern'. Must be a string.")
+      }
+    } else if key == "what" {
+      var what string
+      what, ok = value.(string)
+      if !ok {
+        return nil, errors.New("Invalid value for 'what'. Must be a string.")
+      }
+      if what == "previous" {
+        result.what = CODECMULTILINE_PREVIOUS
+      } else if what == "next" {
+        result.what = CODECMULTILINE_NEXT
+      } else {
+        return nil, errors.New("Invalid value for 'what'. Must be either 'previous' or 'next'.")
       }
     } else if key == "negate" {
       result.negate, ok = value.(bool)
       if !ok {
-        return nil, errors.New("Invalid value for 'negate'.")
+        return nil, errors.New("Invalid value for 'negate'. Must be true or false.")
       }
     } else {
       return nil, errors.New(fmt.Sprintf("Invalid property for multiline codec, '%s'.", key))
@@ -58,20 +75,23 @@ func (c *CodecMultiline) Teardown() int64 {
 }
 
 func (c *CodecMultiline) Event(line uint64, text *string) {
-  if !c.config.negate == c.matcher.MatchString(*text) {
-    if len(c.buffer) != 0 {
-      c.flush()
-      c.buffer = nil
-    }
+  if c.config.what == CODECMULTILINE_PREVIOUS && !c.config.negate == c.matcher.MatchString(*text) {
+    c.flush()
   }
   if len(c.buffer) == 0 {
     c.line = line
   }
   c.offset = c.harvester.Offset
   c.buffer = append(c.buffer, *text)
+  if c.config.what == CODECMULTILINE_NEXT && !c.config.negate == c.matcher.MatchString(*text) {
+    c.flush()
+  }
 }
 
 func (c *CodecMultiline) flush() {
+  if len(c.buffer) == 0 {
+    return
+  }
   text := strings.Join(c.buffer, "\n")
 
   event := &FileEvent{
@@ -87,4 +107,5 @@ func (c *CodecMultiline) flush() {
 
   // Set last offset - this is returned in Teardown so if we're mid multiline and crash, we start this multiline again
   c.last_offset = c.offset
+  c.buffer = nil
 }
