d4b091151731b8bca02c8555061b34d6afaf0c9a
Driskell <devel@jasonwoods.me.uk>
2014-06-13 20:59:20 +0100

    Licence headers and LICENSE

--- lib/logstash/outputs/lumberjack.rb
+++ lib/logstash/outputs/lumberjack.rb
@@ -1,5 +1,22 @@
 # encoding: utf-8
 
+# Copyright 2014 Jason Woods.
+#
+# This file is a modification of code from Logstash Forwarder.
+# Copyright 2012-2013 Jordan Sissel and contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Send events using the lumberjack protocol
 class LogStash::Outputs::Lumberjack < LogStash::Outputs::Base
   config_name "lumberjack"
@@ -29,11 +46,10 @@ class LogStash::Outputs::Lumberjack < LogStash::Outputs::Base
   public
   def register
     require 'lumberjack/client'
-    connect
 
     @client = Lumberjack::Client.new(
       :addresses          => @hosts,
-      :port               => @port, 
+      :port               => @port,
       :ssl_ca             => @ssl_ca,
       :ssl_certificate    => @ssl_certificate,
       :ssl_key            => @ssl_key,
