36d83c98b69cd76b0c208c824b965c07d9b4a82f
Driskell <devel@jasonwoods.me.uk>
2014-06-21 12:30:21 +0100

    Fix server validation on go 1.3

--- src/log-courier/transport_tls.go
+++ src/log-courier/transport_tls.go
@@ -165,6 +165,9 @@ Connect:
         break
       }
 
+      // Set the tlsconfig server name for server validation (since Go 1.3)
+      t.config.tls_config.ServerName = host
+
       t.socket = tls.Client(&TransportTlsWrap{transport: t, tcpsocket: tcpsocket}, &t.config.tls_config)
       t.socket.SetDeadline(time.Now().Add(t.net_config.Timeout))
       err = t.socket.Handshake()
