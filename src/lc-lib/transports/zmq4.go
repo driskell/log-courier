b96281bd507d29ba0969160529515281c24da271
Driskell <devel@jasonwoods.me.uk>
2014-10-29 19:22:22 +0000

    Tidy zmq naming and fix for 4.x

--- src/lc-lib/transports/zmq4.go
+++ src/lc-lib/transports/zmq4.go
@@ -18,10 +18,20 @@
 
 package transports
 
+/*
+#cgo pkg-config: libzmq
+#include <zmq.h>
+#include <zmq_utils.h>
+*/
+import "C"
+
 import (
+  "encoding/binary"
   "fmt"
   zmq "github.com/alecthomas/gozmq"
   "lc-lib/core"
+  "syscall"
+  "unsafe"
 )
 
 func (f *TransportZmqFactory) processConfig(config_path string) (err error) {
@@ -117,6 +127,28 @@ func (t *TransportZmq) processMonitorIn() (ok bool) {
       t.event.part++
     }
   }
+}
+
+func z85Validate(z85 string) bool {
+  var decoded []C.uint8_t
+
+  if len(z85)%5 != 0 {
+    return false
+  } else {
+    // Avoid literal floats
+    decoded = make([]C.uint8_t, 8*len(z85)/10)
+  }
+
+  // Grab a CString of the z85 we need to decode
+  c_z85 := C.CString(z85)
+  defer C.free(unsafe.Pointer(c_z85))
+
+  // Because gozmq does not yet expose this for us, we have to expose it ourselves
+  if ret := C.zmq_z85_decode(&decoded[0], c_z85); ret == nil {
+    return false
+  }
+
+  return true
 }
 
 // Register the transport
