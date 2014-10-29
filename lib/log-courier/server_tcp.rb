9a002f417442dda7797455f73b7c611483b53d90
Driskell <devel@jasonwoods.me.uk>
2014-10-29 16:30:52 +0000

    @logger can be nil

--- lib/log-courier/server_tcp.rb
+++ lib/log-courier/server_tcp.rb
@@ -100,7 +100,7 @@ module LogCourier
         end
 
         if @options[:port] == 0
-          @logger.warn '[LogCourierServer] Transport ' + @options[:transport] + ' is listening on ephemeral port ' + @port.to_s
+          @logger.warn '[LogCourierServer] Transport ' + @options[:transport] + ' is listening on ephemeral port ' + @port.to_s unless @logger.nil?
         end
       rescue => e
         raise "[LogCourierServer] Failed to initialise: #{e}"
