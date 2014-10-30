d46faa38508c3bce2f1960d06337efb59b3e6207
Driskell <devel@jasonwoods.me.uk>
2014-10-30 13:00:06 +0000

    Replace multiline rspec tests with go tests

--- src/lc-lib/codecs/multiline_test.go
+++ src/lc-lib/codecs/multiline_test.go
@@ -3,6 +3,7 @@ package codecs
 import (
   "lc-lib/core"
   "testing"
+  "time"
 )
 
 var gt *testing.T
@@ -25,30 +26,135 @@ func createCodec(unused map[string]interface{}, callback core.CodecCallbackFunc,
 func checkMultiline(start_offset int64, end_offset int64, text string) {
   lines++
 
-  if text != "DEBUG First line\nsecond line\nthird line" {
-    gt.Logf("Event data incorrect [% X]", text)
-    gt.FailNow()
-  }
+  if lines == 1 {
+    if text != "DEBUG First line\nNEXT line\nANOTHER line" {
+      gt.Logf("Event data incorrect [% X]", text)
+      gt.FailNow()
+    }
 
-  if end_offset != 5 {
-    gt.Logf("Event end offset is incorrect [%d]", end_offset)
+    if end_offset != 5 {
+      gt.Logf("Event end offset is incorrect [%d]", end_offset)
+      gt.FailNow()
+    }
+  } else {
+    if text != "DEBUG Next line" {
+      gt.Logf("Event data incorrect [% X]", text)
+      gt.FailNow()
+    }
+
+    if end_offset != 7 {
+      gt.Logf("Event end offset is incorrect [%d]", end_offset)
+      gt.FailNow()
+    }
+  }
+}
+
+func TestMultilinePrevious(t *testing.T) {
+  gt = t
+  lines = 0
+
+  codec := createCodec(map[string]interface{}{
+    "pattern": "^(ANOTHER|NEXT) ",
+    "what": "previous",
+    "negate": false,
+  }, checkMultiline, t)
+
+  // Send some data
+  codec.Event(0, 1, "DEBUG First line")
+  codec.Event(2, 3, "NEXT line")
+  codec.Event(4, 5, "ANOTHER line")
+  codec.Event(6, 7, "DEBUG Next line")
+
+  if lines != 1 {
+    gt.Logf("Wrong line count received")
     gt.FailNow()
   }
 }
 
-func TestMultiline(t *testing.T) {
+func TestMultilinePreviousNegate(t *testing.T) {
   gt = t
   lines = 0
 
   codec := createCodec(map[string]interface{}{
     "pattern": "^DEBUG ",
+    "what": "previous",
     "negate": true,
   }, checkMultiline, t)
 
   // Send some data
   codec.Event(0, 1, "DEBUG First line")
-  codec.Event(2, 3, "second line")
-  codec.Event(4, 5, "third line")
+  codec.Event(2, 3, "NEXT line")
+  codec.Event(4, 5, "ANOTHER line")
+  codec.Event(6, 7, "DEBUG Next line")
+
+  if lines != 1 {
+    gt.Logf("Wrong line count received")
+    gt.FailNow()
+  }
+}
+
+func TestMultilinePreviousTimeout(t *testing.T) {
+  gt = t
+  lines = 0
+
+  codec := createCodec(map[string]interface{}{
+    "pattern": "^(ANOTHER|NEXT) ",
+    "what": "previous",
+    "negate": false,
+    "previous timeout": "5s",
+  }, checkMultiline, t)
+
+  // Send some data
+  codec.Event(0, 1, "DEBUG First line")
+  codec.Event(2, 3, "NEXT line")
+  codec.Event(4, 5, "ANOTHER line")
+  codec.Event(6, 7, "DEBUG Next line")
+
+  // Allow 7 seconds
+  time.Sleep(7 * time.Second)
+
+  if lines != 2 {
+    gt.Logf("Wrong line count received")
+    gt.FailNow()
+  }
+}
+
+func TestMultilineNext(t *testing.T) {
+  gt = t
+  lines = 0
+
+  codec := createCodec(map[string]interface{}{
+    "pattern": "^(DEBUG|NEXT) ",
+    "what": "next",
+    "negate": false,
+  }, checkMultiline, t)
+
+  // Send some data
+  codec.Event(0, 1, "DEBUG First line")
+  codec.Event(2, 3, "NEXT line")
+  codec.Event(4, 5, "ANOTHER line")
+  codec.Event(6, 7, "DEBUG Next line")
+
+  if lines != 1 {
+    gt.Logf("Wrong line count received")
+    gt.FailNow()
+  }
+}
+
+func TestMultilineNextNegate(t *testing.T) {
+  gt = t
+  lines = 0
+
+  codec := createCodec(map[string]interface{}{
+    "pattern": "^ANOTHER ",
+    "what": "next",
+    "negate": true,
+  }, checkMultiline, t)
+
+  // Send some data
+  codec.Event(0, 1, "DEBUG First line")
+  codec.Event(2, 3, "NEXT line")
+  codec.Event(4, 5, "ANOTHER line")
   codec.Event(6, 7, "DEBUG Next line")
 
   if lines != 1 {
