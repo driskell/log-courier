4e2b774d3bc24d3d0d7cf6820c61a715f13a8005
Jason Woods <devel@jasonwoods.me.uk>
2016-02-20 19:02:37 +0000

    Fix #242

--- lc-lib/prospector/prospector.go
+++ lc-lib/prospector/prospector.go
@@ -314,8 +314,14 @@ func (p *Prospector) scan(path string, config *config.File) {
 		resume := !info.isRunning()
 		if resume {
 			if info.status == statusResume {
-				// This is a filestate that was saved, resume the harvester
-				log.Info("Resuming harvester on a previously harvested file: %s", file)
+				if info.finishOffset == fileinfo.Size() && time.Since(fileinfo.ModTime()) > config.DeadTime {
+					// Old file with an unchanged offset, skip it
+					log.Info("Skipping file (older than dead time of %v): %s", config.DeadTime, file)
+					info.status = statusOk
+				} else {
+					// This is a filestate that was saved, resume the harvester
+					log.Info("Resuming harvester on a previously harvested file: %s", file)
+				}
 			} else if info.status == statusFailed {
 				// Last attempt we failed to start, try again
 				log.Info("Attempting to restart failed harvester: %s", file)
