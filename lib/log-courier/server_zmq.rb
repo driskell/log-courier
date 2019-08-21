0f4f794dc41cf10b730b0c9065defa52558e88ed
Jason Woods <devel@jasonwoods.me.uk>
2019-08-21 22:46:07 +0100

    Fix crash on 7.3

--- lib/log-courier/server_zmq.rb
+++ lib/log-courier/server_zmq.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-# Copyright 2014 Jason Woods.
+# Copyright 2014-2019 Jason Woods and Contributors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
