32dc0fa5dacf505adddfff112102d2167efed8b9
Jason Woods <devel@jasonwoods.me.uk>
2021-02-07 17:31:09 +0000

    RPM/DEB build improvements Cross compiling in Ubuntu for 1.15 go builds

--- .github/actions/ppa/entrypoint.sh
+++ .github/actions/ppa/entrypoint.sh
@@ -4,30 +4,54 @@ set -eo pipefail
 
 VERSION=${VERSION#refs/tags/}
 RELEASE=1
-DRELEASE=1
+DRELEASE=${DRELEASE}
 
 echo "::group::Checking exists in $VERSION"
 if [ "${NAME}" != "log-courier" ] && [ ! -d "${NAME}" ]; then
 	exit 0
 fi
 echo '::endgroup::'
-ln -nsf . .master
-echo "::group::Generating sources for $VERSION"
-git archive --format=tar.gz --output ~/"${NAME}_${VERSION#v}.orig.tar.gz" --prefix "${NAME}/" "$VERSION"
-tar -C ~ -xzf ~/"${NAME}_${VERSION#v}.orig.tar.gz"
-cd ~/"${NAME}"
-go mod vendor
-cd -
-tar -C ~ -czf ~/"${NAME}_${VERSION#v}.orig.tar.gz" "${NAME}"
-echo '::endgroup::'
+
+if [ "${DRELEASE}" != 1 ]; then
+	echo "::group::Downloading original source for ${VERSION#v}-${RELEASE} from previous release"
+	wget -q "https://launchpad.net/~devel-k/+archive/ubuntu/log-courier2/+sourcefiles/log-courier/${VERSION#v}-${RELEASE}~trusty$(( DRELEASE - 1 ))/log-courier_${VERSION#v}.orig.tar.gz" -O ~/"${NAME}_${VERSION#v}.orig.tar.gz"
+	echo '::endgroup::'
+else
+	echo "::group::Generating sources for $VERSION"
+	git archive --format=tar.gz --output ~/"${NAME}_${VERSION#v}.orig.tar.gz" --prefix "${NAME}/" "$VERSION"
+	tar -C ~ -xzf ~/"${NAME}_${VERSION#v}.orig.tar.gz"
+	echo '::endgroup::'
+	echo '::group::Adding vendored modules'
+	cd ~/"${NAME}"
+	go mod vendor
+	cd -
+	echo '::endgroup::'
+	echo '::group::Precompiling via cross-compilation as no Go version exists in PPA that is suitable for all distributions'
+	cd ~/"${NAME}"
+	# Configure platform specific defaults
+	export LC_DEFAULT_CONFIGURATION_FILE=/etc/log-courier/log-courier.yaml
+	export LC_DEFAULT_GENERAL_PERSIST_DIR=/var/lib/log-courier
+	export LC_DEFAULT_ADMIN_BIND=unix:/var/run/log-courier/admin.socket
+	go generate -mod=vendor . ./lc-admin ./log-carver
+	mkdir "$(pwd)/bin-i386" "$(pwd)/bin-amd64"
+	GOARCH=386 go build -mod=vendor -o "$(pwd)/bin-i386" . ./lc-admin ./log-carver ./lc-tlscert
+	GOARCH=amd64 go build -mod=vendor -o "$(pwd)/bin-amd64" . ./lc-admin ./log-carver ./lc-tlscert
+	cd -
+	echo '::endgroup::'
+	echo '::group::Updating sources'
+	# Clear cache after vendoring, so that the subsequent test DEB build does not try to use a VCS cache
+	# This will allow us to detect vendoring issues as we will then see it attempt to download additional items
+	go clean -cache -modcache -i -r
+	tar -C ~ -czf ~/"${NAME}_${VERSION#v}.orig.tar.gz" "${NAME}"
+	echo '::endgroup::'
+fi
 
 echo '::group::Installing secrets'
 base64 -id <<<"$GNU_PG" | gpg --batch --import
-gpg --list-secret-keys
-gpg --list-keys
+gpg --import-ownertrust <<EOF
+CF713BECBB9DA51E892E8AD0117FF0FC7420BA3F:6:
+EOF
 echo '::endgroup::'
-
-touch ~/.sbuildrc
 
 for DIST in trusty xenial bionic eoan focal; do
 	echo "::group::Preparing debian package for $DIST"
@@ -42,9 +66,10 @@ for DIST in trusty xenial bionic eoan focal; do
 		cp -rf "/github/workspace/.master/contrib/ppa/${NAME}-systemd" debian
 	fi
 	debchange \
+		--create \
+		--package "${NAME}" \
 		--newversion "${VERSION#v}-${RELEASE}~${DIST}${DRELEASE}" \
-		--allow-lower-version "${VERSION#v}-${RELEASE}~*" \
-		--distribution "$DIST" \
+		--distribution "${DIST}" \
 		--controlmaint \
 		"Package for ${DIST}"
 	echo '::endgroup::'
@@ -58,6 +83,6 @@ for DIST in trusty xenial bionic eoan focal; do
 	echo '::endgroup::'
 
 	echo "::group::Submitting package"
-	#dput ppa:devel-k/log-courier2 "../${NAME}_${VERSION#v}-${RELEASE}~${DIST}${DRELEASE}_source.changes"
+	dput ppa:devel-k/log-courier2 "../${NAME}_${VERSION#v}-${RELEASE}~${DIST}${DRELEASE}_source.changes"
 	echo '::endgroup::'
 done
--- .github/actions/rpm/entrypoint.sh
+++ .github/actions/rpm/entrypoint.sh
@@ -13,9 +13,18 @@ echo '::endgroup::'
 echo "::group::Generating sources for $VERSION"
 mkdir -p ~/rpmbuild/{SOURCES,SPECS}
 git archive --format=zip --output ~/"rpmbuild/SOURCES/$VERSION.zip" --prefix "log-courier-${VERSION#v}/" "$VERSION"
+echo '::endgroup::'
+
+echo "::group::Adding vendored modules to source"
 go mod vendor
+# Clear cache after vendoring, so that the subsequent test RPM build does not try to use a VCS cache
+# This will allow us to detect vendoring issues as we will then see it attempt to download additional items
+go clean -cache -modcache -i -r
 ln -nsf . "log-courier-${VERSION#v}"
 zip -qr ~/"rpmbuild/SOURCES/$VERSION.zip" "log-courier-${VERSION#v}/vendor"
+echo '::endgroup::'
+
+echo "::group::Generating spec for $VERSION"
 sed "s/Version: %%VERSION%%/Version: ${VERSION#v}/" <".master/contrib/rpm/${NAME}.spec" >~/"rpmbuild/SPECS/${NAME}.spec"
 echo '::endgroup::'
 
