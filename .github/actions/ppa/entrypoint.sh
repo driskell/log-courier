b04c297af68acc5a00b3b9bdfcf362ed22d60710
Jason Woods <devel@jasonwoods.me.uk>
2021-02-07 01:29:47 +0000

    Resolve PPA issues

--- .github/actions/ppa/entrypoint.sh
+++ .github/actions/ppa/entrypoint.sh
@@ -11,27 +11,35 @@ if [ "${NAME}" != "log-courier" ] && [ ! -d "${NAME}" ]; then
 	exit 0
 fi
 echo '::endgroup::'
-
+ln -nsf . .master
 echo "::group::Generating sources for $VERSION"
 git archive --format=tar.gz --output ~/"${NAME}_${VERSION#v}.orig.tar.gz" --prefix "${NAME}/" "$VERSION"
-go mod vendor
-tar -rzf ~/"${NAME}_${VERSION#v}.orig.tar.gz" vendor
 tar -C ~ -xzf ~/"${NAME}_${VERSION#v}.orig.tar.gz"
+cd ~/"${NAME}"
+go mod vendor
+cd -
+tar -C ~ -czf ~/"${NAME}_${VERSION#v}.orig.tar.gz" "${NAME}"
 echo '::endgroup::'
 
 echo '::group::Installing secrets'
 base64 -id <<<"$GNU_PG" | gpg --batch --import
+gpg --list-secret-keys
+gpg --list-keys
 echo '::endgroup::'
 
-cd ~/"${NAME}"
+touch ~/.sbuildrc
 
 for DIST in trusty xenial bionic eoan focal; do
 	echo "::group::Preparing debian package for $DIST"
+	rm -rf ~/"${NAME}"
+	cd ~
+	tar -xzf ~/"${NAME}_${VERSION#v}.orig.tar.gz"
+	cd ~/"${NAME}"
 	rm -rf debian
 	if [ "$DIST" == "trusty" ]; then
-		cp -rf ".master/contrib/ppa/${NAME}-upstart" debian
+		cp -rf "/github/workspace/.master/contrib/ppa/${NAME}-upstart" debian
 	else
-		cp -rf ".master/contrib/ppa/${NAME}-systemd" debian
+		cp -rf "/github/workspace/.master/contrib/ppa/${NAME}-systemd" debian
 	fi
 	debchange \
 		--newversion "${VERSION#v}-${RELEASE}~${DIST}${DRELEASE}" \
@@ -41,11 +49,15 @@ for DIST in trusty xenial bionic eoan focal; do
 		"Package for ${DIST}"
 	echo '::endgroup::'
 
-	echo "::group::Building package"
+	echo "::group::Building source"
 	debuild -d -S -sa
 	echo '::endgroup::'
 
+	echo "::group::Testing DEB"
+	debuild -d -b
+	echo '::endgroup::'
+
 	echo "::group::Submitting package"
-	dput ppa:devel-k/log-courier2 "../${NAME}_${VERSION#v}-${RELEASE}~${DIST}${DRELEASE}_source.changes"
+	#dput ppa:devel-k/log-courier2 "../${NAME}_${VERSION#v}-${RELEASE}~${DIST}${DRELEASE}_source.changes"
 	echo '::endgroup::'
 done
