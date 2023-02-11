ff05b8b8f28af9669aa2b616182205f3910a7995
Jason Woods <devel@jasonwoods.me.uk>
2023-02-11 13:25:56 +0000

    chore: Fixup test framework for ruby and update versions tested against

--- ruby/setup_logstash.sh
+++ ruby/setup_logstash.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+set -eo pipefail
+
 if [ -d logstash ]; then
 	rm -rf logstash
 fi
 
 git clone -b v8.6.1 https://github.com/elastic/logstash.git logstash --depth 1
+
+(
+	cd logstash
+	rake bootstrap
+)
