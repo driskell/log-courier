2f6d77d558546f47e66c08712a28b3677543550a
Jason Woods <devel@jasonwoods.me.uk>
2024-02-26 14:53:36 +0000

    fix: Resolve a formatting issue in Handshake failure output

--- lc-lib/transports/tcp/connectionsockettls.go
+++ lc-lib/transports/tcp/connectionsockettls.go
@@ -62,7 +62,7 @@ func (t *connectionSocketTLS) Setup(ctx context.Context) error {
 
 	err := t.Handshake()
 	if err != nil {
-		log.Warning("[%s %s - %s] TLS handshake failed: %w", side, t.LocalAddr().String(), t.RemoteAddr().String(), err)
+		log.Warning("[%s %s - %s] TLS handshake failed: %s", side, t.LocalAddr().String(), t.RemoteAddr().String(), err.Error())
 		return err
 	}
 
